29e65dd54c265dbc0f0b28f5e3a58de695bf0720
Diego Montaluisa <santitodiego12@gmail.com>
2025-06-30 20:23:10 -0500

    final

--- src/Interfas/BancoForm.java
+++ src/Interfas/BancoForm.java
@@ -1,3 +1,5 @@
+package Interfas;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- src/Login/LoginForm.java
+++ src/Login/LoginForm.java
@@ -1,3 +1,7 @@
+package Login;
+
+import Interfas.BancoForm;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
